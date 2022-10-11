5267236304681c403945b480dd561e5fbf387700
designer-mehedi <mehedihasan80912@gmail.com>
2022-10-11 18:38:14 +0600

    navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,16 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div className='bg-indigo-600'>
-            <NavLink to="/home" className={({isActive}) => isActive ? 'active' : undefined}>Home</NavLink>
-            <NavLink to="/statistics">Statistics</NavLink>
-            <NavLink to="/blog">Blog</NavLink>
-        </div>
+        <header className='container px-32 bg-indigo-600 flex justify-between text-white py-5'>
+            <div className="nav-brand font-bold">
+                <h1>Quiz World</h1>
+            </div>
+            <nav className='font-semibold navbar uppercase'>
+                <NavLink to="/home" className={({ isActive }) => isActive ? 'active' : undefined}>Home</NavLink>
+                <NavLink to="/statistics">Statistics</NavLink>
+                <NavLink to="/blog">Blog</NavLink>
+            </nav>
+        </header>
     );
 };
 
